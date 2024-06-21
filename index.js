b62c63cbbb0cd760a7137fcfea945646d9a75c62
MeeraOmar22 <123479441+MeeraOmar22@users.noreply.github.com>
2024-06-21 21:14:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'June 21, 2024',
+  birthdate: 'June 22, 2024',
   name: 'Khairy'
 };
 
